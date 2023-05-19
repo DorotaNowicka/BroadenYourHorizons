dad8d0151462a23bae11d4a0fd301229ef8e4687
DorotaNowicka <d.nowicka@student.uw.edu.pl>
2023-05-19 13:40:05 +0200

    Add properly display tables

--- src/components/DisplayList.js
+++ src/components/DisplayList.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import useGetCountries from "../api/useGetCountries";
 import { ApolloClient, InMemoryCache, gql, useQuery } from "@apollo/client";
 import CountryInfoTable from "./countryTable";
+import { key } from "localforage";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const GET_COUNTRIES_BY_CONTINENT = gql`
   query GetCountriesByContinent($continentCode: String!) {
@@ -29,11 +31,14 @@ const fetchData = async (name) => {
 };
 
 function DisplayList(props) {
-  const continent = "EU";
-  const number = 3;
+  const navigation = useNavigate();
+  const location = useLocation();
+  const continent = location.state.continent;
+  const number = location.state.number;
+  console.log(continent, number);
   const [isReady, setIsReady] = useState(false);
-  const [randomCountries, setRandomCountries] = useState({ countries: "" });
-  const [countryItems, initCountry] = useState([]);
+  const [randomCountries, setRandomCountries] = useState({});
+  const [countryItems, initCountry] = useState({});
   const {
     data: countriesFromContinent,
     loading,
@@ -56,43 +61,64 @@ function DisplayList(props) {
       ).map((country) => country.name);
       console.log(chosenCountries);
 
-      chosenCountries.forEach((country) => {
-        fetchData(country)
-          .then((res) => {
-            countryItems[country] = res;
+      // chosenCountries.forEach((country) => {
+      //   fetchData(country)
+      //     .then((res) => {
+      //       countryItems[country] = res[0];
+      //     })
 
-            //initCountry((countryItems) => countryItems.push(res));
-          })
+      //     .catch((e) => {
+      //       console.log(e.message);
+      //     });
+      // });
 
-          .catch((e) => {
-            console.log(e.message);
+      Promise.all(chosenCountries.map((country) => fetchData(country)))
+        .then((responses) => {
+          const updatedCountryItems = {};
+          responses.forEach((res, index) => {
+            updatedCountryItems[chosenCountries[index]] = res[0];
           });
-      });
-      console.log("na koniec", countryItems);
-      console.log(countryItems);
+          initCountry((prevCountryItems) => ({
+            ...prevCountryItems,
+            ...updatedCountryItems,
+          }));
+        })
+        .catch((e) => {
+          console.log(e.message);
+        });
+
+      // console.log("keys", Object.keys(countryItems));
+
       setIsReady(true);
+      console.log("CI", countryItems);
+      console.log("ready", isReady);
     }
     return () => {
+      console.log("na koniec", countryItems, Object.keys(countryItems));
       console.log("cleaned");
     };
   }, [loading, error, countriesFromContinent]);
 
   if (loading || error) {
-    return <h1>Loading...</h1>;
+    return <h1>Loading....</h1>;
   }
+
   return (
     <div className="DiscoverPage">
       <h1 style={{ padding: "10px" }}>Country Details</h1>
-      {!isReady && <h2>Loading countries details...</h2>}
-      <div className="Grid">
-        {countryItems.map((item) => {
-          return (
-            <div className="TableDetails" key={item[0].name.common}>
-              <CountryInfoTable country={item[0]} />
-            </div>
-          );
-        })}
-      </div>
+      {!isReady ? (
+        <h2>Loading countries details...</h2>
+      ) : (
+        <div className="Grid">
+          {Object.keys(countryItems).map((item) => {
+            return (
+              <div className="TableDetails" key={item}>
+                <CountryInfoTable country={countryItems[item]} />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
